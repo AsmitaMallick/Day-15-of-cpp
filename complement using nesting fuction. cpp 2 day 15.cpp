f27b7133d4f79bedbce41d6e34c65020bf58206a
ASMITA MALLICK <asmitamallick2@gmail.com>
2023-03-16 13:29:03 +0530

    cpp

---
+++ complement using nesting fuction. cpp 2 day 15.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class binary{
+	string s;
+	public:
+		void read(void);
+		void chk_bin(void);
+		void ones(void);
+		void display(void);
+	
+};
+
+
+void binary :: read(void){
+	cout<<"enter a binary number"<<endl;
+	cin>>s;
+}
+void binary :: chk_bin(void){
+	for(int i=0;i<s.length();i++){
+		if(s.at(i)!='0' && s.at(i)!='1'){
+			cout<<"incorrect binary"<<endl;
+			exit(0);
+		}
+	}
+}
+void binary :: ones(void){
+	for(int i=0;i<s.length();i++){
+		if (s.at(i)=='0'){
+			s.at(i)='1';
+	}
+	if (s.at(i)=='1'){
+		s.at(i)='0';
+	
+}
+}
+}
+void binary :: display(void){
+	cout<<"\n displaying the binary"<<endl;
+	for(int i=0;i<s.length();i++){
+		cout<<s.at(i);
+	}
+	
+}
+int main(){
+	binary b;
+	b.read();
+	b.chk_bin();
+	b.display();
+	b.ones();
+	b.display();
+}
+
+
